3114b0cbb5954da77c2c8d8620d1351c946171a5
SeongUkSeo <45838097+SeongUkSeo@users.noreply.github.com>
2023-09-05 13:12:55 +0900

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,71 +1,86 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sensor Dashboard',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SensorDashboard(),
+      home: HomeScreen(),
     );
   }
 }
 
-class SensorDashboard extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
   @override
-  _SensorDashboardState createState() => _SensorDashboardState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _SensorDashboardState extends State<SensorDashboard> {
-  List<dynamic> sensorData = [];
+class _HomeScreenState extends State<HomeScreen> {
+  File? _backgroundImage;
 
-  @override
-  void initState() {
-    super.initState();
-    fetchSensorData();
-  }
+  _pickImage() async {
+    final pickedFile = await ImagePicker().getImage(source: ImageSource.gallery);
 
-  Future<void> fetchSensorData() async {
-    final response = await http.get(Uri.parse('API_URL'));
-    if (response.statusCode == 200) {
+    if (pickedFile != null) {
       setState(() {
-        sensorData = json.decode(response.body);
+        _backgroundImage = File(pickedFile.path);
       });
-    } else {
-      throw Exception('Failed to fetch sensor data');
     }
-  }
-
-  Future<void> refreshSensorData() async {
-    await fetchSensorData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sensor Dashboard'),
+        title: Text('Room App'),
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {
+            // Drawer를 여는 코드 추가
+          },
+        ),
       ),
-      body: ListView.builder(
-        itemCount: sensorData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: ListTile(
-              title: Text('Sensor ${index + 1}'),
-              subtitle: Text('Value: ${sensorData[index]['value']}'),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            ListTile(
+              title: Text("Room 1"),
+              onTap: () {
+                Navigator.pop(context);
+                // Room 1 선택 처리
+              },
             ),
-          );
-        },
+            // 다른 Room들 추가
+            ListTile(
+              title: Icon(Icons.add),
+              onTap: () {
+                Navigator.pop(context);
+                // Room 추가 처리
+              },
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: refreshSensorData,
-        child: Icon(Icons.refresh),
+      body: GestureDetector(
+        onTap: _pickImage,
+        child: Container(
+          decoration: BoxDecoration(
+            image: _backgroundImage == null
+                ? DecorationImage(
+                    image: AssetImage('assets/add_icon.png'),
+                    fit: BoxFit.cover,
+                  )
+                : DecorationImage(
+                    image: FileImage(_backgroundImage!),
+                    fit: BoxFit.cover,
+                  ),
+          ),
+        ),
       ),
     );
   }
